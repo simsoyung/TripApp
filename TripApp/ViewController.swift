2dd032a63407af7ef6eefb217703b3c1ddffc88b
soyung sim <simsy1024@gmail.com>
2024-05-27 19:35:32 +0900

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by 심소영 on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
